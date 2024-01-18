d3d9c7e7aa487304d3bb5b296608967fc9a99242
mrtaanvir <mdtanvirhossain0445@gmail.com>
2024-01-18 12:53:23 +0600

    first commit

---
+++ P-100(3n+1).c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int len(int n) {
+    int count = 0;
+    while (n != 1) {
+        if (n % 2 != 0) {
+            n = (3 * n) + 1;
+        } else {
+            n = n / 2;
+        }
+        count++;
+    }
+    return count+1; // Corrected return statement
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    int a, b;
+    while (scanf("%d %d", &a, &b) != EOF) {
+        if (a < b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int mx = 0;
+        while (a >= b) {
+            int num = len(a);
+            mx = max(mx, num);
+            a--;
+        }
+        printf("%d\n", mx); // Corrected printf statement
+    }
+
+    return 0;
+}
